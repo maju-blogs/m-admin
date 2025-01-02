668e904864406ac54404b78c56157a88eb2bb8ad
maju-blogs <1099874269@qq.com>
2025-01-02 14:28:11 +0800

    Update PublicConstant.java

--- m-common/src/main/java/org/m/common/constant/PublicConstant.java
+++ m-common/src/main/java/org/m/common/constant/PublicConstant.java
@@ -5,6 +5,6 @@ public class PublicConstant {
     public static final String MQTT_CLIENT_OUT = "/out";
     public static final long EXECUTE_NORMAL_TIME = 5000;
 
-    public static final int PAY_TASK_DELAY_TIME = 120;
+    public static final int PAY_TASK_DELAY_TIME = 30;
     public static final String MAX_SECOND = ":59";
 }
